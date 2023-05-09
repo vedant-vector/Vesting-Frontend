75685758ad2e3a930625ea4ab2dbc6f7acfc80e8
vedant-simform <vedant@simformsolutions.com>
2023-05-09 15:49:23 +0530

    Build Version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,48 +5,69 @@ import { ethers } from "ethers";
 
 const Header = () => {
   const [isConnected, setConnection] = useState(false);
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useState(null);
+  const [reload, setReload] = useState(false);
 
   let signer;
   useEffect(() => {
-    const ad = localStorage.getItem("Account");
+    let ad = localStorage.getItem("Account");
+
     const connection = localStorage.getItem("Connection");
     if (ad) {
+      ad =
+        localStorage.getItem("Account").slice(0, 6) +
+        "..." +
+        localStorage.getItem("Account").slice(-4);
       setAddress(ad);
     }
     if (connection === "true") {
       setConnection(true);
     }
-  }, []);
+  }, [reload]);
 
   const walletConnect = async () => {
     try {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       await provider.send("eth_requestAccounts", []);
       signer = provider.getSigner();
-      setAddress(await signer.getAddress());
+      let ad = await signer.getAddress();
+      ad = ad.slice(0, 6) + "..." + ad.slice(-4);
+      setAddress(ad);
       setConnection(true);
+      const copyToClipboard = async () => {
+        navigator.clipboard.writeText(await signer.getAddress());
+      };
+      copyToClipboard();
       localStorage.setItem("Account", await signer.getAddress());
       localStorage.setItem("Connection", true);
     } catch (err) {
       if (err.code === 4001) {
         setConnection(false);
-        setAddress("");
-        localStorage.setItem("Connection", false);
-        localStorage.removeItem("Account");
+        setAddress(null);
       }
+      localStorage.setItem("Connection", false);
+      localStorage.removeItem("Account");
+
+      setReload(true);
+      window.location.reload();
     }
   };
 
-  window.ethereum.on("accountsChanged", walletConnect);
+  try {
+    window.ethereum.on("accountsChanged", walletConnect);
+  } catch (error) {
+    alert("Metamask Wallet Not Found!!  ");
+  }
 
   return (
     <div className="h-18 border-b-2 shadow-for-bg shadow-2xl flex">
-      <img
-        src={logo}
-        alt="Vesting Vector"
-        className=" h-20 ml-10 mt-2 px-6 py-3 "
-      />
+      <Link to={"/home"}>
+        <img
+          src={logo}
+          alt="Vesting Vector"
+          className=" h-20 ml-10 mt-2 px-6 py-3 "
+        />
+      </Link>
 
       <ul className="flex m-auto mr-2 text-xl pl-2">
         <Link to={"/home"}>
@@ -60,6 +81,7 @@ const Header = () => {
       <button
         onClick={walletConnect}
         className=" w-40 px-3 text-xl h-auto m-auto mx-10 overflow-hidden rounded-3xl bg-for-bg text-white"
+        title="Click to copy address"
       >
         {isConnected
           ? address
